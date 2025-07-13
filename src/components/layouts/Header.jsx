f85ba29b06325cda33506664ee70f8565dddc492
codayon <reshadahammedayon@proton.me>
2025-07-13 19:42:04 +0600

    Add header and icons

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import Container from "../Container";
+import Flex from "../Flex";
+import { Link } from "react-router";
+import Logo from "../../assets/icons/Logo";
+import Search from "../../assets/icons/Search";
+import User from "../../assets/icons/User";
+import Heart from "../../assets/icons/Heart";
+import Cart from "../../assets/icons/Cart";
+import Bar from "../../assets/icons/Bar";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <section className="py-8">
+      <Container>
+        <Flex className={"justify-between"}>
+          <div>
+            <Logo />
+          </div>
+          <div>
+            <ul className="flex gap-x-10 font-medium text-sm text-">
+              <Link to={"/"}>
+                <li className="sliding-underline px-2 py-1.5">HOME</li>
+              </Link>
+              <Link to={"shop"}>
+                <li className="sliding-underline px-2 py-1.5">SHOP</li>
+              </Link>
+              <Link to={"collection"}>
+                <li className="sliding-underline px-2 py-1.5">COLLECTION</li>
+              </Link>
+              <Link to={"journal"}>
+                <li className="sliding-underline px-2 py-1.5">JOURNAL</li>
+              </Link>
+              <Link to={"lookbook"}>
+                <li className="sliding-underline px-2 py-1.5">LOOKBOOK</li>
+              </Link>
+              <Link to={"pages"}>
+                <li className="sliding-underline px-2 py-1.5">PAGES</li>
+              </Link>
+            </ul>
+          </div>
+          <Flex className={"gap-x-8"}>
+            <div className="w-5 h-5">
+              <Link to={"/"}>
+                <Search />
+              </Link>
+            </div>
+            <div className="w-5 h-5">
+              <Link to={"/"}>
+                <User />
+              </Link>
+            </div>
+            <div className="w-5 h-5">
+              <Link to={"/"}>
+                <Heart />
+              </Link>
+            </div>
+            <div className="w-5 h-5">
+              <Link to={"/"}>
+                <Cart />
+              </Link>
+            </div>
+            <div className="w-5 h-5">
+              <Link to={"/"}>
+                <Bar />
+              </Link>
+            </div>
+          </Flex>
+        </Flex>
+      </Container>
+    </section>
+  );
+};
 
-export default Header
+export default Header;
