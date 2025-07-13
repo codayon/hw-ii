f0c75c803587b2b54ad148963bab2a55914e0e2f
codayon <reshadahammedayon@proton.me>
2025-07-13 16:45:20 +0600

    fix: Add index routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import Journal from "./pages/Journal";
 import Lookbook from "./pages/Lookbook";
 import Pages from "./pages/Pages";
 import Shop from "./pages/Shop";
+import RootLayout from "./components/layouts/RootLayout";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<Home />} />
-      <Route path="shop" element={<Shop />} />
-      <Route path="collection" element={<Collection />} />
-      <Route path="journal" element={<Journal />} />
-      <Route path="lookbook" element={<Lookbook />} />
-      <Route path="pages" element={<Pages />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="collection" element={<Collection />} />
+        <Route path="journal" element={<Journal />} />
+        <Route path="lookbook" element={<Lookbook />} />
+        <Route path="pages" element={<Pages />} />
+      </Route>
     </Routes>
   );
 }
